90b5405fe0050d2a7ff0e33e3aeca5156d4e6ae8
CamGham <greayham@gmail.com>
2022-10-19 18:30:40 +1300

    simple changes

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -2,11 +2,12 @@ import { Button, TextField } from "@mui/material";
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import {signInWithEmailAndPassword} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import "./LoginForm.css";
 
 const LoginForm = (props) => {
-const auth = props.auth;
-const navigate = props.navigate;
+  const auth = props.auth;
+  const navigate = props.navigate;
 
   const formik = useFormik({
     initialValues: {
@@ -21,41 +22,49 @@ const navigate = props.navigate;
       //perform login
 
       signInWithEmailAndPassword(auth, values.email, values.password)
-      .then((userCredentials) =>{
-        const user = userCredentials.user;
-        navigate("/home");
-      })
-      .catch((error) =>{
-    alert("Invalid login" + error);
-      })
+        .then((userCredentials) => {
+          const user = userCredentials.user;
+          navigate("/home");
+        })
+        .catch((error) => {
+          alert("Invalid login" + error);
+        });
     },
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} className="loginForm">
-      <div className="fieldCont">
-        <TextField
-          id="email"
-          variant="outlined"
-          label="Email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        <TextField
-          id="password"
-          variant="outlined"
-          type="password"
-          label="Password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        <Button id="login" type="submit" variant="contained">
-          Login
-        </Button>
-      </div>
-    </form>
+    <div>
+      <form onSubmit={formik.handleSubmit} className="loginForm">
+        <div className="fieldCont">
+          <div className="compCont">
+            <TextField
+              id="email"
+              variant="outlined"
+              label="Email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+          </div>
+          <div className="compCont">
+            <TextField
+              id="password"
+              variant="outlined"
+              type="password"
+              label="Password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+          </div>
+          <div className="compCont">
+            <Button id="login" type="submit" variant="contained">
+              Login
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
