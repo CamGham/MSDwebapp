cdf35d9cc03d31f8afaedd2c861956290e4f5aea
CamGham <greayham@gmail.com>
2022-10-23 22:02:46 +1300

    analysis modal

---
+++ frontend/src/components/AnalysisView.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Modal } from '@mui/material'
+import "./ModalView.css"
+
+const AnalysisView = (props) => {
+    const show = props.show;
+  return (
+    <div>
+        <Modal
+        open={show}
+        >
+            
+            <h1>
+                Loading...
+            </h1>
+        </Modal>
+    </div>
+  )
+}
+
+export default AnalysisView
